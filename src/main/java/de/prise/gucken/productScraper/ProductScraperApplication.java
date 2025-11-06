8af6c6094e23735012319703668ea37190384555
zaheer <zaheer@gmx.at>
2025-11-06 13:01:24 +0100

    Add project to github

---
+++ src/main/java/de/prise/gucken/productScraper/ProductScraperApplication.java
@@ -0,0 +1,13 @@
+package de.prise.gucken.productScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/prise/gucken/productScraper/ProductScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package de.prise.gucken.productScraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
